7201cf8885f8169b1ef1fa3619114f8dec238166
Garrett Foster <garrett.d.foster@gmail.com>
2021-07-21 16:41:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+
+    //set defaults
+    try {
+        $('select[name="signup"]').val(getCookie('DR_scrim_signup'));
+        $('select[name="openclose"').val(getCookie('DR_scrim_openclose'));
+        $('select[name="scrimstart"]').val(getCookie('DR_scrim_scrimstart'));
+        $('select[name="leagueboxes"]').val(getCookie('DR_scrim_leagueboxes'));
+        post("http://deeproute.com/scrim2.asp").then(
+            function (e) {
+                document.getElementById("result").innerHTML = e.target.responseText;
+                let val = getCookie('DR_scrim_teamz');
+                console.log(val);
+                $('select[name="teamz"]').val(val);
+            }, function (e) {
+                // handle errors
+            });
+    } catch {
+        console.log('default scrimmage values not set yet');
+    }
+
+
+    //store values
+    $('input.btn-primary').click(function () {
+        $('select').each(function (i, el) {
+            let name = 'DR_scrim_' + $(el).attr('name');
+            let val = $(el).val();
+            setCookie(name, val, 7);
+        });
+    });
+
+    console.log('ready');
+});
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function post(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = resolve;
+        xhr.onerror = reject;
+        xhr.send('lgno=' + escape(document.forms['scrim'].leagueboxes.value));
+    });
+}
